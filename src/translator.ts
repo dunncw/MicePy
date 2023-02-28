ec58992714c5a8c485f0ed0a77fd6392d5d7b3ee
dunncw <dunncw@g.cofc.edu>
2023-02-27 22:49:57 -0500

    got the translator working in the extension

--- src/translator.ts
+++ src/translator.ts
@@ -4,27 +4,33 @@ async function translate(text: string): Promise<string> {
   const url = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-es";
   const apiKey = "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";
 
+  // create the body data for the request
+  // it should grab the text from the input box and put it in the body wrapped in quotes
+
+  // make the request
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Authorization": `Bearer ${apiKey}`,
-      "Content-Type": "application/json"
+      "Content-Type": 'application/json'
     },
-    body: JSON.stringify({
-      inputs: text
-    })
+    body: JSON.stringify({inputs: text})
   });
+
+  // print out the structure of the response
+  console.log(response);
 
 
   if (!response.ok) {
     throw new Error(`Translation failed with status ${response.status}: ${await response.text()}`);
   }
 
-  console.log(response);
-
-  const { outputs } = await response.json() as { outputs: string };
+  const outputs = await response.json();
   console.log(outputs);
-  return outputs;
+
+  const translated_text = outputs[0].translation_text;
+  console.log(translated_text);
+  return translated_text;
 }
 
 export { translate };
